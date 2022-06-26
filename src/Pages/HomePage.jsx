d2f6104a8491992d044a3ea1c28e5a69091f030c
gaurav sharma <g11.sharma@gmail.com>
2022-06-26 21:30:15 +0200

    changed Lorem ipsum text in home page

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -21,9 +21,12 @@ const HomePage = () => {
         <img src={newbear} />
         <h1>New Beer</h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad minus ab
-          aperiam commodi esse quas porro dicta, sint laboriosam non sequi
-          ipsam, suscipit possimus fugiat ex repellat eligendi debitis earum!
+          Lorem ipsum dolor sit amet. Et perspiciatis aperiam ex consequatur est
+          unde soluta eos debitis sunt. Et harum voluptatem est iusto quia ut
+          iure architecto est dolorum consequuntur 33 incidunt labore eos
+          voluptatum error et voluptatem officiis. Quo modi fuga ut amet quia
+          qui omnis totam eos dolorem voluptas et nisi rerum et cupiditate
+          totam.
         </p>
         <Link to="./new-beer">New Beer</Link>
       </div>
@@ -31,9 +34,12 @@ const HomePage = () => {
         <img src={randomBear} />
         <h1>Random-Bear</h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad minus ab
-          aperiam commodi esse quas porro dicta, sint laboriosam non sequi
-          ipsam, suscipit possimus fugiat ex repellat eligendi debitis earum!
+          Lorem ipsum dolor sit amet. Ex dolores odio ut dolores molestiae rem
+          quasi sint vel eligendi expedita cum animi nesciunt rem dicta dolore
+          est dicta voluptatibus. Qui vitae sunt ut rerum reiciendis ut
+          aspernatur corrupti ab nesciunt quae sed ipsa officiis! Et minima
+          magnam est quae internos rem corrupti possimus sit quam dolorem et
+          sint excepturi aut commodi cupiditate.
         </p>
         <Link to="./random-beer">Random-Bear</Link>
       </div>
